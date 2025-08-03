ced60a0b33745a70bab091ab3e40260f0b1a8a4d
Paul Mann <paulmann2205@gmail.com>
2025-08-03 18:51:20 +0200

    Added docstrings to all functions

--- src/database/DatabaseCollection.py
+++ src/database/DatabaseCollection.py
@@ -2,14 +2,31 @@ import os
 from database.Database import Database
 
 class DatabaseCollection:
-    database_folder_path = ""
+    """
+    A collection of databases.
+    """
+
     listDatabase: Database = None
 
     def __init__(self,database_folder_path: str):
-        self.database_folder_path = database_folder_path
-        self.listDatabase: Database = Database(self.database_folder_path,"list",[("name","TEXT"), ("user","TEXT"),("date","DATE"),("console","TEXT"),("rating","INT"),("genre","TEXT"),("review","TEXT"),("cover","TEXT"),("replay","INTEGER DEFAULT 0"),("hundred_percent","INTEGER DEFAULT 0")])
+        """
+        Initializes the DatabaseCollection with a specified folder path where the databases should be created.
+        If the folder does not exist, it will be created.
+        Will also initialize the databases used. (Currently only the list database is initialized.)
+        :param database_folder_path: The path to the folder where the databases will be stored.
+        """
+        self.create_database_folder_if_not_exists(database_folder_path)
+
+        self.__database_folder_path = database_folder_path
+        self.listDatabase: Database = Database(self.__database_folder_path,"list",[("name","TEXT"), ("user","TEXT"),("date","DATE"),("console","TEXT"),("rating","INT"),("genre","TEXT"),("review","TEXT"),("cover","TEXT"),("replay","INTEGER DEFAULT 0"),("hundred_percent","INTEGER DEFAULT 0")])
 
     @staticmethod
     def create_database_folder_if_not_exists(database_folder_path: str):
+        """
+        Creates the database folder if it does not exist.
+        This is necessary to ensure that the databases can be created in the specified folder otherwise it would throw an error.
+        :param database_folder_path: Path to the folder where the databases should be created.
+        """
         if not os.path.exists(database_folder_path):
+            print("Creating database folder at: " + database_folder_path)
             os.makedirs(database_folder_path)
--- src/listbot/Bot.py
+++ src/listbot/Bot.py
@@ -7,18 +7,33 @@ from database.DatabaseCollection import DatabaseCollection
 from discord.ext import commands
 
 class Bot:
+    """
+    A Discord bot that manages a list of games.
+    """
     __config_data: dict = None
     __intents: discord.Intents = discord.Intents.default()
     __bot: commands.bot = None
-    _databases:DatabaseCollection = DatabaseCollection("../resources/databases/")
+    _databases:DatabaseCollection = None
 
     @property
     def databases(self) -> DatabaseCollection:
+        """
+        Returns the collection of databases used by the bot.
+        :return: the DatabaseCollection instance containing the bot's databases.
+        """
         return self._databases
 
     def __init__(self, command_prefix: str,config_path: str):
+        """
+        Initializes the bot.
+        The bot will create a resources directory if it does not exist,
+        and will load the configuration from the specified path.
+        :param command_prefix: The prefix used for bot commands.
+        :param config_path: The path to the configuration file for the bot.
+        """
         self.create_resources_directory_if_not_exists()
         self.__config_data = self.load_config(config_path)
+        self._databases = DatabaseCollection(self.__config_data['bot']['databases_folder_path'])
 
         self.__intents = self.set_intents()
         self.__bot = commands.Bot(command_prefix=self.__config_data['bot']['command_prefix'], intents=self.__intents)
@@ -28,6 +43,10 @@ class Bot:
             print(f"Bot is ready! Logged in as {self.__bot.user.name} (ID: {self.__bot.user.id})")
 
     def run(self):
+        """
+        Runs the bot using the API key from the configuration file.
+        This method will block until the bot is stopped.
+        """
         api_key = self.__config_data["bot"]["api_key"]
 
         try:
@@ -37,6 +56,11 @@ class Bot:
 
     @staticmethod
     def set_intents() -> discord.Intents:
+        """
+        Sets the intents for the bot.
+        This method enables the message content intent, which is required for the bot to read message content.
+        :return: New discord.Intents object with set intents.
+        """
         intents = discord.Intents.default()
         intents.message_content = True
 
@@ -44,6 +68,12 @@ class Bot:
 
     @staticmethod
     def load_config(config_path: str) -> dict:
+        """
+        Loads the configuration from the specified path.
+        If the configuration file does not exist, it will be created with default values.
+        :param config_path: The path to the configuration file.
+        :return: The data loaded from the configuration file as a dictionary.
+        """
         Bot.create_config_if_not_exists(config_path=config_path)
 
         with open(config_path,'r') as file:
@@ -51,12 +81,23 @@ class Bot:
 
     @staticmethod
     def create_resources_directory_if_not_exists():
+        """
+        Creates the resources directory if it does not exist.
+        This directory is used to store the configuration file and is supposed to contain the databases(But can be changed in the config).
+        """
         if not os.path.exists("../resources/"):
+            print("Creating resources directory at: ../resources/")
             os.mkdir("../resources/")
 
     @staticmethod
     def create_config_if_not_exists(config_path: str):
+        """
+        Creates a configuration file at the specified path if it does not exist.
+        The configuration file will contain default values.
+        :param config_path: The path where the configuration file should be created.
+        """
         if not os.path.exists(config_path):
+            print(f"Creating configuration file at: {config_path}")
             with open(config_path, "w") as file:
                 file.write("# Configuration file for the Discord bot\n")
                 file.write("\n")
@@ -64,4 +105,5 @@ class Bot:
                 file.write("bot:\n")
                 file.write("  api_key:\n")
                 file.write("  command_prefix: '%'\n")
+                file.write("  databases_folder_path: '../resources/databases/'\n")
                 file.write("  accepted_users:\n")
