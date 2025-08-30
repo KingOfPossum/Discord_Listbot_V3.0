bcfb106d4210dd46724f17b4bcca2b945802d060
Paul Mann <paulmann2205@gmail.com>
2025-08-30 16:29:30 +0200

    Added support for display_names

--- src/common/UserManager.py
+++ src/common/UserManager.py
@@ -1,5 +1,4 @@
-import discord
-
+from discord import Member
 from common.ConfigLoader import ConfigLoader
 from discord.ext.commands import Bot
 
@@ -8,7 +7,7 @@ class UserManager:
     A class to manage user-related operations in the Discord bot.
     This class is responsible for handling things like if a user is even able to do something or if he is ignored by the bot.
     """
-    accepted_users: set[str] = set()
+    accepted_users: set[Member] = set()
 
     @staticmethod
     def init(bot: Bot):
@@ -23,21 +22,47 @@ class UserManager:
         if accepted_users_config is None:
             UserManager.accepted_users = set()
         elif accepted_users_config == {"all"}:
-            UserManager.accepted_users = {member.name for guild in bot.guilds for member in guild.members}
+            UserManager.accepted_users = {member for guild in bot.guilds for member in guild.members}
         else:
-            UserManager.accepted_users = set(accepted_users_config)
+            UserManager.accepted_users = {member for guild in bot.guilds for member in guild.members if (
+                        member.name in accepted_users_config or member.display_name in accepted_users_config)}
 
         print(f"UserManager initialized with accepted users: {UserManager.accepted_users}")
 
     @staticmethod
-    def is_user_accepted(user_name:str) -> bool:
+    def is_user_accepted(user_name: str) -> bool:
         """
         Checks if a user is accepted to use the bot.
         :param user_name: The name of the user to check.
         :return: True if user is accepted, False otherwise.
         """
-        if user_name in UserManager.accepted_users:
-            return True
-        else:
-            print(f"User {user_name} is not accepted to use the bot.")
-            return False
+        for member in UserManager.accepted_users:
+            if member.name == user_name or member.display_name == user_name:
+                return True
+
+        print(f"User {user_name} is not accepted to use the bot.")
+        return False
+
+    @staticmethod
+    def get_display_name(user_name: str) -> str | None:
+        """
+        Gets the display name of a user by their username.
+        :param user_name: The name of the user to get the display name for.
+        :return: The display name of the user, or None if the user is not found.
+        """
+        for member in UserManager.accepted_users:
+            if member.name == user_name or member.display_name == user_name:
+                return member.display_name
+        return None
+
+    @staticmethod
+    def get_user_name(display_name: str) -> str | None:
+        """
+        Gets the username of a user by their display name.
+        :param display_name: The display name of the user to get the username for.
+        :return: The username of the user, or None if the user is not found.
+        """
+        for member in UserManager.accepted_users:
+            if member.name == display_name or member.display_name == display_name:
+                return member.name
+        return None
