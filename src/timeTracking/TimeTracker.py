bc2ef00ed36106188b425698be4ee130604e51b8
Paul Mann <paulmann2205@gmail.com>
2025-10-08 19:09:22 +0200

    Updated TimeTracker to use TimeEntry objects

--- src/timeTracking/TimeTracker.py
+++ src/timeTracking/TimeTracker.py
@@ -1,3 +1,4 @@
+from common.TimeEntry import TimeEntry
 from common.UserManager import UserManager
 from database.TimeDatabase import TimeDatabase
 from discord.ext import commands,tasks
@@ -38,17 +39,15 @@ class TimeTracker(commands.Cog):
 
         for user in UserManager.accepted_users:
             if user.activity is None:
-                print(f"User {user.name} has no activity")
                 continue
 
-            print(f"User {user.name} is playing {user.activity.name}")
             if self.tracking_dict[user.name]["current_activity"] == user.activity.name:
                 try:
                     self.tracking_dict[user.name]["activities"][user.activity.name] += 10
-                    self.time_database.put_entry(user.name,user.activity.name, self.tracking_dict[user.name]["activities"][user.activity.name],is_new=False)
                 except KeyError:
                     self.tracking_dict[user.name]["activities"][user.activity.name] = 10
-                    self.time_database.put_entry(user.name,user.activity.name, self.tracking_dict[user.name]["activities"][user.activity.name],is_new=True)
+                new_entry = TimeEntry(user.name,user.activity.name,self.tracking_dict[user.name]["activities"][user.activity.name])
+                self.time_database.put_entry(new_entry)
             self.tracking_dict[user.name]["current_activity"] = user.activity.name
 
         print(self.tracking_dict)
@@ -80,7 +79,8 @@ class TimeTracker(commands.Cog):
         new_dict = {}
         for user in users:
             new_dict[user] = {"current_activity": None, "activities": {}}
-            for activity in entries[user]:
-                new_dict[user]["activities"][activity[0]] = activity[1]
+
+        for entry in entries:
+            new_dict[entry.user]["activities"][entry.activity] = entry.time_spent
 
         self.tracking_dict = new_dict
