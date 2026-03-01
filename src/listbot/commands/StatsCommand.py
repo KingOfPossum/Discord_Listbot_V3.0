bc7e3f59413163324cd2bc03b924d2b8a5acd705
Paul Mann <paulmann2205@gmail.com>
2026-03-01 19:16:25 +0100

    Changed processing of stats data to use sql instead of processing with python

--- src/listbot/commands/StatsCommand.py
+++ src/listbot/commands/StatsCommand.py
@@ -3,15 +3,12 @@ import discord
 from common.Command import Command
 from common.ConfigLoader import ConfigLoader
 from common.Emojis import Emojis
-from common.GameEntry import GameEntry
 from common.MessageManager import MessageManager
 from common.TimeUtils import TimeUtils
 from common.UserManager import UserManager
+from database.DatabaseCollection import DatabaseCollection
 from discord import Embed
 from discord.ext import commands
-
-from database.DatabaseCollection import DatabaseCollection
-
 
 class StatsCommand(Command):
     """Command to view statistics about added games in the list and users."""
@@ -24,28 +21,19 @@ class StatsCommand(Command):
             return
 
         year = TimeUtils.get_current_year()
-        game_entries, highest_rated_games, worst_rated_games, user_game_counts, months_counts, console_counts = self.get_stats(year=year)
-
-        embed = self.get_main_page_embed(year,user_game_counts, highest_rated_games, worst_rated_games, months_counts, console_counts)
+        embed = self.get_main_page_embed(year=year)
 
         async def back_callback(interaction: discord.Interaction):
-            nonlocal year,game_entries, highest_rated_games, worst_rated_games, user_game_counts, months_counts, console_counts
-            game_entries, highest_rated_games, worst_rated_games, user_game_counts, months_counts, console_counts = self.get_stats(year=year)
-
-            new_embed = self.get_main_page_embed(year,user_game_counts, highest_rated_games, worst_rated_games, months_counts, console_counts)
-            await interaction.response.edit_message(embed=new_embed,view=standard_view)
+            await interaction.response.edit_message(embed=self.get_main_page_embed(year),view=standard_view)
 
         async def consoles_callback(interaction: discord.Interaction):
-            new_embed = self.get_consoles_stats_embed(year,console_counts)
-            await interaction.response.edit_message(embed=new_embed,view=standard_view)
+            await interaction.response.edit_message(embed=self.get_consoles_stats_embed(year),view=standard_view)
 
         async def ratings_callback(interaction: discord.Interaction):
-            new_embed = self.get_ratings_stats_embed(year,game_entries)
-            await interaction.response.edit_message(embed=new_embed,view=standard_view)
+            await interaction.response.edit_message(embed=self.get_ratings_stats_embed(year),view=standard_view)
 
         async def months_callback(interaction: discord.Interaction):
-            new_embed = self.get_months_stats_embed(year,months_counts)
-            await interaction.response.edit_message(embed=new_embed,view=standard_view)
+            await interaction.response.edit_message(embed=self.get_months_stats_embed(year),view=standard_view)
 
         async def users_callback(interaction: discord.Interaction):
             await interaction.response.edit_message(embed=embed,view=user_stats_view)
@@ -83,18 +71,18 @@ class StatsCommand(Command):
                 user_button = discord.ui.Button(label=user.display_name,style=discord.ButtonStyle.gray)
 
                 async def user_callback(interaction: discord.Interaction,current_user=user):
-                    nonlocal game_entries, highest_rated_games, worst_rated_games, user_game_counts, months_counts, console_counts
-                    game_entries, highest_rated_games, worst_rated_games, user_game_counts, months_counts, console_counts = self.get_stats(user=current_user.name)
-
-                    new_embed = self.get_user_stats_embed(current_user.display_name,highest_rated_games, worst_rated_games, months_counts, console_counts)
-                    await interaction.response.edit_message(embed=new_embed, view=user_stats_view)
+                    await interaction.response.edit_message(embed=self.get_user_stats_embed(current_user.id,year=year), view=user_stats_view)
 
                 user_button.callback = user_callback
                 user_stats_view.add_item(user_button)
 
         year_buttons = {}
         years = DatabaseCollection.list_database.get_years()
+
+        # Insert None at the beginning of the list to represent the total stats
         years.insert(0,None)
+
+        # Create a button for each year and add it to the standard view. When a button is clicked, the main page embed is updated to show the stats for the selected year. And the button is disabled to indicate that it is the currently selected year.
         for _year in years:
             year_button = discord.ui.Button(label=_year if _year is not None else "Total",style=discord.ButtonStyle.gray)
 
@@ -103,17 +91,14 @@ class StatsCommand(Command):
                 year_buttons[_year].disabled = True
 
             async def year_callback(interaction: discord.Interaction,current_year=_year):
-                nonlocal year,game_entries, highest_rated_games, worst_rated_games, user_game_counts, months_counts, console_counts
+                nonlocal year
                 year = current_year
-                game_entries, highest_rated_games, worst_rated_games, user_game_counts, months_counts, console_counts = self.get_stats(year=current_year)
-
-                new_embed = self.get_main_page_embed(year,user_game_counts, highest_rated_games, worst_rated_games, months_counts, console_counts)
 
                 for yb in year_buttons.values():
                     yb.disabled = False
                 year_buttons[current_year].disabled = True
 
-                await interaction.response.edit_message(embed=new_embed, view=standard_view)
+                await interaction.response.edit_message(embed=self.get_main_page_embed(year), view=standard_view)
 
             year_button.callback = year_callback
             standard_view.add_item(year_button)
@@ -124,101 +109,86 @@ class StatsCommand(Command):
         """Returns a string that describes the command and how to use it."""
         return f"- `{ConfigLoader.get_config().command_prefix}stats` - View statistics.\n"
 
-    def get_stats(self,user=None,year=None) -> tuple:
-        """
-        Gets statistics about added games in the list.
-        Will return the following statistics:
-        - Highest Rated Games
-        - Worst Rated Games
-        - Most Active Users
-        - Most Active Months
-        - Most Used Consoles
-        :param user: The user to filter the statistics. If None, consider all users.
-        :param year: The year to filter the statistics. If None, consider all years.
-        :return: A tuple containing lists of statistics.
-        """
-        user_entry = UserManager.get_user_entry(user_name=user)
-        game_entries = DatabaseCollection.list_database.get_all_game_entries(user_id=user_entry.user_id if user_entry else None,year=year)
-        print(game_entries)
-
-        highest_rated_games = [(entry.name, entry.rating) for entry in
-                               sorted(game_entries, key=lambda x: x.rating, reverse=True)[:3]]
-        worst_rated_games = [(entry.name, entry.rating) for entry in sorted(game_entries, key=lambda x: x.rating)[:3]]
-        user_game_counts = self.get_user_game_counts(game_entries)[:3]
-        months_counts = self.get_months_counts(game_entries)
-        console_counts = self.get_console_counts(game_entries)[:3]
-
-        return game_entries,highest_rated_games, worst_rated_games, user_game_counts, months_counts, console_counts
-
-    def get_main_page_embed(self, year,user_game_counts, highest_rated_games, worst_rated_games, months_counts, console_counts) -> Embed:
+    def get_main_page_embed(self, year:int = None) -> Embed:
         """
         Creates the main stats embed which shows up to three entries for each stat.
         :param year: The year for which the stats are shown.
-        :param user_game_counts: The amount of games added per user.
-        :param highest_rated_games: The highest rated games.
-        :param worst_rated_games: The worst rated games.
-        :param months_counts: The amount of games added per month.
-        :param console_counts: The amount of games added per console.
         :return: the main stats embed.
         """
-        embed = discord.Embed(title=f"Stats ({"Total" if year is None else year})", color=0x00ff00)
+        user_game_counts = DatabaseCollection.list_database.get_user_game_counts(year=year,limit=3)
+        highest_rated_games = DatabaseCollection.list_database.get_highest_rated_games(year=year,limit=3)
+        worst_rated_games = DatabaseCollection.list_database.get_worst_rated_games(year=year,limit=3)
+        months_counts = DatabaseCollection.list_database.get_months_counts(year=year)
+        console_counts = DatabaseCollection.list_database.get_console_counts(year=year,limit=3)
+        genre_counts = DatabaseCollection.list_database.get_genre_counts(year=year,limit=3)
+
+        embed = discord.Embed(title=f"Stats ({year if year else "Total"})", color=0x00ff00)
         embed.add_field(name="", value=f"**Games Played:**\n{self.convert_to_string(user_game_counts)}\n"
                                        f"**Highest Rated Games:**\n{self.convert_to_string(highest_rated_games)}\n"
                                        f"**Worst Rated Games:**\n{self.convert_to_string(worst_rated_games)}\n")
         embed.add_field(name="", value=f"**Most Used Console:**\n{self.convert_to_string(console_counts)}\n"
-                                       f"**Most Active Month:**\n{self.convert_to_string(months_counts)}\n")
+                                       f"**Most Active Month:**\n{self.convert_to_string(months_counts)}\n"
+                                       f"**Most Played Genre:**\n{self.convert_to_string(genre_counts)}\n")
         return embed
 
-    def get_user_stats_embed(self,user_name,highest_rated_games,worst_rated_games,months_count,console_counts) -> Embed:
+    def get_user_stats_embed(self,user_id:int,year:int = None) -> Embed:
         """
-        Creates the user sats embed which shows up to three entries for each stat.
-        :param user_name: The user for which the stats are shown.
-        :param highest_rated_games: The highest rated games.
-        :param worst_rated_games: The worst rated games.
-        :param months_count: The amount of games added per month.
-        :param console_counts: The amount of games added per console.
+        Creates the user stats embed which shows up to three entries for each stat.
+        :param user_id: The ID of the user for which the stats are shown.
+        :param year: The year for which the stats are shown.
         :return: The user stats embed.
         """
-        embed = discord.Embed(title=f"{user_name}'s Stats",color=0x00ff00)
+        highest_rated_games = DatabaseCollection.list_database.get_highest_rated_games(user_id=user_id,year=year,limit=3)
+        worst_rated_games = DatabaseCollection.list_database.get_worst_rated_games(user_id=user_id,year=year,limit=3)
+        console_counts = DatabaseCollection.list_database.get_console_counts(user_id=user_id,year=year,limit=3)
+        months_counts = DatabaseCollection.list_database.get_months_counts(user_id=user_id,year=year,limit=3)
+        genre_counts = DatabaseCollection.list_database.get_genre_counts(user_id=user_id,year=year,limit=3)
+
+        embed = discord.Embed(title=f"{UserManager.get_user_entry(user_id=user_id).display_name}'s Stats ({year if year else "Total"})",color=0x00ff00)
         embed.add_field(name="",value=f"**Highest Rated Games:**\n{self.convert_to_string(highest_rated_games)}\n"
                                       f"**Worst Rated Games:**\n{self.convert_to_string(worst_rated_games)}\n")
         embed.add_field(name="",value=f"**Most Used Console:**\n{self.convert_to_string(console_counts)}\n"
-                                      f"**Most Active Month:**\n{self.convert_to_string(months_count)}")
+                                      f"**Most Active Month:**\n{self.convert_to_string(months_counts)}\n"
+                                      f"**Most Played Genre:**\n{self.convert_to_string(genre_counts)}")
         return embed
 
-    def get_consoles_stats_embed(self,year,console_counts) -> Embed:
+    def get_consoles_stats_embed(self,year) -> Embed:
         """
         Creates the consoles stats embed which shows the top 10 most used consoles.
         :param year: The year for which the stats are shown.
-        :param console_counts: The amount of games added per console.
         :return: The consoles stats embed.
         """
-        embed = discord.Embed(title=f"Most Used Consoles ({"Total" if year is None else year})", color=0x00ff00)
-        consoles_txt = "\n".join([f"- {entry[0]} : {entry[1]}" for entry in console_counts[:10]])
+        embed = discord.Embed(title=f"Most Used Consoles ({year if year else "Total"})", color=0x00ff00)
+        console_counts = DatabaseCollection.list_database.get_console_counts(year=year,limit=10)
+
+        consoles_txt = "\n".join([f"- {entry[0]} : {entry[1]}" for entry in console_counts])
         embed.add_field(name="", value=consoles_txt)
 
         return embed
 
-    def get_ratings_stats_embed(self,year,game_entries) -> Embed:
+    def get_ratings_stats_embed(self,year) -> Embed:
         """
         Creates the ratings stats embed which shows the top 10 highest rated games.
         :param year: The year for which the stats are shown.
         :return: The ratings stats embed.
         """
-        embed = discord.Embed(title=f"Highest Rated Games ({"Total" if year is None else year})", color=0x00ff00)
-        ratings_txt = "\n".join([f"- {entry[0]} : {entry[1]}" for entry in [(entry.name, entry.rating) for entry in sorted(game_entries, key=lambda x: x.rating,reverse=True)[:10]]])
+        embed = discord.Embed(title=f"Highest Rated Games ({year if year else "Total"})", color=0x00ff00)
+        highest_rated_games = DatabaseCollection.list_database.get_highest_rated_games(year=year,limit=10)
+
+        ratings_txt = "\n".join([f"- {entry[0]} : {entry[1]}" for entry in highest_rated_games])
         embed.add_field(name="", value=ratings_txt)
 
         return embed
 
-    def get_months_stats_embed(self,year,months_counts) -> Embed:
+    def get_months_stats_embed(self,year) -> Embed:
         """
         Creates the months stats embed which shows the top 10 most active months.
         :param year: The year for which the stats are shown.
-        :param months_counts: The amount of games added per month.
         :return: The months stats embed.
         """
-        embed = discord.Embed(title=f"Most Active Months ({"Total" if year is None else year})", color=0x00ff00)
+        embed = discord.Embed(title=f"Most Active Months ({year if year else "Total"})", color=0x00ff00)
+        months_counts = DatabaseCollection.list_database.get_months_counts(year=year,limit=10)
+
         months_txt = "\n".join([f"- {entry[0]} : {entry[1]}" for entry in months_counts])
         embed.add_field(name="", value=months_txt)
 
@@ -237,55 +207,3 @@ class StatsCommand(Command):
                 break
             txt += f"{Emojis.RANKINGS[i]} {entries[i][0]} : { entries[i][1]}\n"
         return txt
-
-    def get_user_game_counts(self,game_entries: list[GameEntry]) -> list[tuple[str, int]]:
-        """
-        Returns the amount of games added per user in a list of tuples
-        :param game_entries: list of all game entries
-        :return list of tuples containing usernames and amount of games added
-        """
-        game_counts = {}
-
-        for entry in game_entries:
-            try:
-                game_counts[entry.user_id] += 1
-            except KeyError:
-                game_counts[entry.user_id] = 1
-
-        return [(UserManager.get_user_entry(user_id=user).display_name, count) for user,count in sorted(game_counts.items(), key=lambda item: item[1], reverse=True)]
-
-    def get_months_counts(self,game_entries: list[GameEntry]) -> list[tuple[str,int]]:
-        """
-        Returns the amount of games added per month  in a list of tuples
-        :param game_entries: list of all game entries
-        :return: list of tuples containing month names and amount of games added
-        """
-        month_counts = {}
-
-        for entry in game_entries:
-            month = entry.date.split("-")[1]
-            MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-            month = MONTHS[int(month)-1]
-
-            try:
-                month_counts[month] += 1
-            except KeyError:
-                month_counts[month] = 1
-
-        return [(month, count) for month,count in sorted(month_counts.items(), key=lambda item: item[1], reverse=True)]
-
-    def get_console_counts(self,game_entries: list[GameEntry]) -> list[tuple[str,int]]:
-        """
-        Returns the amount of games added per console in a list of tuples
-        :param game_entries: list of all game entries
-        :return: list of tuples containing console names and amount of games added
-        """
-        console_counts = {}
-
-        for entry in game_entries:
-            try:
-                console_counts[entry.console] += 1
-            except KeyError:
-                console_counts[entry.console] = 1
-
-        return [(console, count) for console,count in sorted(console_counts.items(), key=lambda item: item[1], reverse=True)]
